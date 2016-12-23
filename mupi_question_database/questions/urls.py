43a39e0fe8349e2e57669d838f1b8f2e6bdcc822
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2016-12-23 10:21:14 -0200

    Add router logic

--- mupi_question_database/questions/urls.py
+++ mupi_question_database/questions/urls.py
@@ -1,83 +1,49 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
 
 from . import views
 from . import rest_views
 
-# #View Sets
-user_list = rest_views.UserViewSet.as_view({
-    'get'   : 'list',
-    'post'  : 'create'
-})
-
-user_detail = rest_views.UserViewSet.as_view({
-    'get'   : 'retrieve',
-    'put'   : 'update',
-    'delete': 'destroy'
-})
-
-question_list = rest_views.QuestionViewSet.as_view({
-    'get'   : 'list',
-    'post'  : 'create'
-})
-
-question_detail = rest_views.QuestionViewSet.as_view({
-    'get'   : 'retrieve',
-    'put'   : 'update',
-    'delete': 'destroy'
-})
-
-questionlist_list = rest_views.Question_ListViewSet.as_view({
-    'get'   : 'list',
-    'post'  : 'create'
-})
-
-questionlist_detail = rest_views.Question_ListViewSet.as_view({
-    'get'   : 'retrieve',
-    'put'   : 'update',
-    'delete': 'destroy'
-})
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'questions', rest_views.QuestionViewSet)
+router.register(r'users', rest_views.UserViewSet)
+router.register(r'question_lists', rest_views.Question_ListViewSet)
 
 
 urlpatterns = [
-    url(r'^$', views.QuestionSearchView.as_view(), name='list'),
-    url(r'^search/$', views.QuestionSearchView.as_view() ,name='question_search'),
-
-    url(r'^search_tag_ajax/$', views.QuestionTagSearchView.as_view()  ,name='question_search_tag_ajax'),
-
-    url(r'^(?P<pk>[\d]+)/$',views.QuestionDetailView.as_view(), name='question_detail'),
-    url(r'^generate_list$',views.list_generator, name='list_generator'),
-    url(r'^generate_answer_list$',views.answer_list_generator, name='answer_list_generator'),
-    url(r'^check_question/$',views.check_question, name='check_question'),
-    url(r'^clear_questions/$',views.clear_questions, name='clear_questions'),
-    url(r'^create_question/$',views.QuestionCreate.as_view(success_url='/questions/create_question'), name='question_form'),
-
-    url(r'^question_list/create$',views.Question_ListCreateView.as_view(), name='question_list_create'),
-    url(r'^question_lists/$',views.Question_ListListView.as_view(), name='question_list_list'),
-    url(r'^question_lists/own$',views.Question_ListOwnListView.as_view(), name='question_list_own_list'),
-    url(r'^question_lists/(?P<pk>[\d]+)/$',views.Question_ListDetailView.as_view(), name='question_list_detail'),
-    url(r'^question_lists/(?P<pk>[\d]+)/delete$',views.Question_ListDeleteView.as_view(), name='question_list_delete'),
-    url(r'^question_lists/(?P<pk>[\d]+)/clone$',views.Question_ListCloneView.as_view(), name='question_list_clone'),
-    url(r'^question_lists/(?P<pk>[\d]+)/edit$',views.Question_ListEditView.as_view(), name='question_list_edit'),
-    url(r'^question_lists/(?P<pk>[\d]+)/edit/questions$',views.QuestionEditSearchView.as_view(), name='question_list_edit_list'),
-    url(r'^question_lists/(?P<pk>[\d]+)/edit/questions/search$',views.QuestionEditSearchView.as_view(), name='question_search_edit_list'),
-    url(r'^question_lists/(?P<pk>[\d]+)/edit/questions/remove$',views.Question_ListRemoveQuestionsView.as_view(), name='question_list_edit_remove_list'),
-
-    url(r'^check_question_edit_add_list/$',views.check_question_edit_add_list, name='check_question_edit_add_list'),
-    url(r'^clear_questions_edit_add_list/$',views.clear_questions_edit_add_list, name='clear_questions_edit_add_list'),
-    url(r'^check_question_edit_list/$',views.check_question_edit_list, name='check_question_edit_list'),
-
-    url(r'^get_tags/$', views.autocomplete, name='get_tags'),
+    # url(r'^$', views.QuestionSearchView.as_view(), name='list'),
+    # url(r'^search/$', views.QuestionSearchView.as_view() ,name='question_search'),
+    #
+    # url(r'^search_tag_ajax/$', views.QuestionTagSearchView.as_view()  ,name='question_search_tag_ajax'),
+    #
+    # url(r'^(?P<pk>[\d]+)/$',views.QuestionDetailView.as_view(), name='question_detail'),
+    # url(r'^generate_list$',views.list_generator, name='list_generator'),
+    # url(r'^generate_answer_list$',views.answer_list_generator, name='answer_list_generator'),
+    # url(r'^check_question/$',views.check_question, name='check_question'),
+    # url(r'^clear_questions/$',views.clear_questions, name='clear_questions'),
+    # url(r'^create_question/$',views.QuestionCreate.as_view(success_url='/questions/create_question'), name='question_form'),
+    #
+    # url(r'^question_list/create$',views.Question_ListCreateView.as_view(), name='question_list_create'),
+    # url(r'^question_lists/$',views.Question_ListListView.as_view(), name='question_list_list'),
+    # url(r'^question_lists/own$',views.Question_ListOwnListView.as_view(), name='question_list_own_list'),
+    # url(r'^question_lists/(?P<pk>[\d]+)/$',views.Question_ListDetailView.as_view(), name='question_list_detail'),
+    # url(r'^question_lists/(?P<pk>[\d]+)/delete$',views.Question_ListDeleteView.as_view(), name='question_list_delete'),
+    # url(r'^question_lists/(?P<pk>[\d]+)/clone$',views.Question_ListCloneView.as_view(), name='question_list_clone'),
+    # url(r'^question_lists/(?P<pk>[\d]+)/edit$',views.Question_ListEditView.as_view(), name='question_list_edit'),
+    # url(r'^question_lists/(?P<pk>[\d]+)/edit/questions$',views.QuestionEditSearchView.as_view(), name='question_list_edit_list'),
+    # url(r'^question_lists/(?P<pk>[\d]+)/edit/questions/search$',views.QuestionEditSearchView.as_view(), name='question_search_edit_list'),
+    # url(r'^question_lists/(?P<pk>[\d]+)/edit/questions/remove$',views.Question_ListRemoveQuestionsView.as_view(), name='question_list_edit_remove_list'),
+    #
+    # url(r'^check_question_edit_add_list/$',views.check_question_edit_add_list, name='check_question_edit_add_list'),
+    # url(r'^clear_questions_edit_add_list/$',views.clear_questions_edit_add_list, name='clear_questions_edit_add_list'),
+    # url(r'^check_question_edit_list/$',views.check_question_edit_list, name='check_question_edit_list'),
+    #
+    # url(r'^get_tags/$', views.autocomplete, name='get_tags'),
 
     # Rest Urls
 
-    url(r'^rest/users/$', user_list),
-
-    url(r'^rest/questions/$', question_list),
-    url(r'^rest/questions/(?P<pk>[0-9]+)/$', question_detail),
-
-    url(r'^rest/question_lists/$', questionlist_list),
-    url(r'^rest/question_lists/(?P<pk>[0-9]+)/$', questionlist_detail),
-
-
+    url(r'^rest/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
