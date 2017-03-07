e3ed89e73c8151e5e36010fa7e89bb051f4cb4dc
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2017-03-07 10:28:07 -0300

    Make all questions avaiable to all users

--- mupi_question_database/questions/rest_views.py
+++ mupi_question_database/questions/rest_views.py
@@ -252,8 +252,8 @@ The question will only be deleted if the current authenticated user is the autho
     def retrieve(self, request, pk=None):
         user = self.request.user
         question = self.get_object()
-        if (user.is_anonymous() or question not in user.profile.avaiable_questions.all()) and not user.is_superuser:
-            self.serializer_class = serializers.QuestionBasicSerializer
+        # if (user.is_anonymous() or question not in user.profile.avaiable_questions.all()) and not user.is_superuser:
+        #     self.serializer_class = serializers.QuestionBasicSerializer
         return super().retrieve(request, pk)
 
     def list(self, request):
