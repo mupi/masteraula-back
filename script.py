92ade35fa595895a8d5cafa1bf42507b1adceac9
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2017-03-10 11:59:38 -0300

    Simple script to populate the database To use it you need to access the virtualenv, run ./manage.py shell and inside the shell execute the folowwing command exec(open(./script.py).read()) This script will read a questions.json file

---
+++ script.py
@@ -0,0 +1,42 @@
+from mupi_question_database.questions.models import Question, Answer
+import json
+
+counter = 1
+try:
+    with open('questions.json') as data_file:
+        data = json.load(data_file)
+        for question_data in data["questions"]:
+            counter = counter + 1
+
+            # verify if the question has one and just one right answer
+            right_answer = False
+            message = ''
+            for answer_data in question_data["answers"]:
+                if answer_data['is_correct'] and not right_answer:
+                    right_answer = True
+                elif answer_data['is_correct'] and right_answer:
+                    message = ('Question ' + str(counter) + ' has two right answer')
+            if not right_answer:
+                print('Question ' + str(counter) + ' dont have right answer')
+                continue
+            if message != '':
+                print(message)
+                continue
+
+            question = Question.objects.create(question_text=question_data["question_text"],
+                                                question_header=question_data["question_header"],
+                                                level=question_data["level"],
+                                                resolution=question_data["resolution"],
+                                                author_id=1)
+
+            for answer_data in question_data["answers"]:
+                answer = Answer.objects.create(answer_text=answer_data["answer_text"],
+                                                    is_correct=answer_data["is_correct"],
+                                                    question_id=question.id)
+
+            for tag in question_data["tags"]:
+                question.tags.add(tag)
+                question.save()
+    print('Done data')
+except:
+    print('Error: Question ' + str(counter))
