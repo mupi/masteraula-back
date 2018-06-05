8f1da7dc283ae0ef172069c67c8af58e4ca60941
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-06-05 11:47:53 -0300

    Disable questions app

--- masteraula/questions/admin.py
+++ masteraula/questions/admin.py
@@ -1,63 +1,61 @@
 from django.contrib import admin
 
-from .models import Question, Answer, Question_List, QuestionQuestion_List, Profile, Subject
+from .models import Subject
 
 
-class QuestionModelAdmin(admin.ModelAdmin):
-    raw_id_fields = ('question_parent', )
+# class SubjectModelAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'name',)
 
-    list_display = ('id', 'question_statement',)
+#     search_fields = ['name',]
 
-    search_fields = ['id', 'question_statement',]
+#     list_per_page = 50
 
-    list_per_page = 50
+# class AlternativeModelAdmin(admin.ModelAdmin):
+#     raw_id_fields = ('question', )
 
-class AnswerModelAdmin(admin.ModelAdmin):
-    raw_id_fields = ('question', )
+#     list_display = ('id', 'question', 'answer_text', 'is_correct')
 
-    list_display = ('id', 'question', 'answer_text', 'is_correct')
+#     search_fields = ['id', 'answer_text', 'question__statement', 'question__id' ]
 
-    search_fields = ['id', 'answer_text', 'question__question_statement', 'question__id' ]
+#     list_per_page = 100
 
-    list_per_page = 100
+# class Question_ListModelAdmin(admin.ModelAdmin):
+#     raw_id_fields = ('owner', )
 
-class Question_ListModelAdmin(admin.ModelAdmin):
-    raw_id_fields = ('cloned_from', 'owner' )
+#     list_display = ('id', 'header', 'owner',)
 
-    list_display = ('id', 'question_list_header', 'owner', 'secret')
+#     search_fields = ['id', 'header', 'owner__name' ]
 
-    search_fields = ['id', 'question_list_header', 'owner__name' ]
+#     list_per_page = 100
 
-    list_per_page = 100
+# class QuestionQuestion_ListModelAdmin(admin.ModelAdmin):
+#     raw_id_fields = ('question', 'question_list' )
 
-class QuestionQuestion_ListModelAdmin(admin.ModelAdmin):
-    raw_id_fields = ('question', 'question_list' )
+#     list_display = ('id', 'question_list', 'question', 'order')
 
-    list_display = ('id', 'question_list', 'question', 'order')
+#     search_fields = ['id', 'question_list__id', 'question_list__name', 'question_list__owner' ]
 
-    search_fields = ['id', 'question_list__id', 'question_list__name', 'question_list__owner' ]
+#     list_per_page = 100
 
-    list_per_page = 100
+# class QuestionsInline(admin.TabularInline):
+#     model = Profile.avaiable_questions.through
+#     raw_id_fields = ('question',)
 
-class QuestionsInline(admin.TabularInline):
-    model = Profile.avaiable_questions.through
-    raw_id_fields = ('question',)
+# class ProfileModelAdmin(admin.ModelAdmin):
+#     raw_id_fields = ('user',)
 
-class ProfileModelAdmin(admin.ModelAdmin):
-    raw_id_fields = ('user',)
+#     list_display = ('id', 'user', 'credit_balance',)
 
-    list_display = ('id', 'user', 'credit_balance',)
+#     search_fields = ['id', 'user__name', ]
 
-    search_fields = ['id', 'user__name', ]
+#     inlines = [QuestionsInline,]
+#     exclude = ('avaiable_questions',)
 
-    inlines = [QuestionsInline,]
-    exclude = ('avaiable_questions',)
+#     list_per_page = 100
 
-    list_per_page = 100
-
-admin.site.register(Question,QuestionModelAdmin)
-admin.site.register(Question_List, Question_ListModelAdmin)
-admin.site.register(QuestionQuestion_List, QuestionQuestion_ListModelAdmin)
-admin.site.register(Answer, AnswerModelAdmin)
-admin.site.register(Profile, ProfileModelAdmin)
-admin.site.register(Subject)
+# admin.site.register(Subject, SubjectModelAdmin)
+# admin.site.register(Question,QuestionModelAdmin)
+# admin.site.register(Document, Question_ListModelAdmin)
+# admin.site.register(DocumentQuestion, QuestionQuestion_ListModelAdmin)
+# admin.site.register(Alternative, AlternativeModelAdmin)
+# admin.site.register(Profile, ProfileModelAdmin)
--- masteraula/questions/models.py
+++ masteraula/questions/models.py
@@ -1,57 +1,77 @@
+# -*- coding: utf-8 -*-
 import datetime
 from django.db import models
-from masteraula.users.models import User
+from django.utils.translation import ugettext_lazy as _
+
 from taggit.managers import TaggableManager
 
+from masteraula.users.models import User
+
 class Subject(models.Model):
-    subject_name = models.CharField(max_length=50, null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
 
     def __str__(self):
-        return u'Subject %s' % (self.subject_name)
+        return self.name
+
+
+class EducationLevel(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class LearningObject(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    image = models.ImageField(null=True, blank=True)
+    text = models.TextField(null=True, blank=True)
+
+    tags = TaggableManager()
+
+    def __str__(self):
+        return self.name
+
+
+class Describer(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name + " " + description[:50]
+
 
 class Question(models.Model):
     LEVEL_CHOICES = (
         ('', 'Nenhuma opção'),
-        ('E', 'Fácil'),
-        ('M', 'Médio'),
-        ('H', 'Difícil')
+        ('E', _('Easy')),
+        ('M', _('Medium')),
+        ('H', _('Hard'))
     )
 
-    EDUCATION_CHOICES = (
-        ('Ensino Médio', 'Ensino Médio'),
-        ('Fundamental II - anos finais', 'Fundamental II - anos finais'),
-        ('Fundamental II - anos iniciais', 'Fundamental II - anos iniciais'),
-        ('Ensino Superior', 'Ensino Superior')
-    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_date = models.DateTimeField(auto_now_add=True)
 
-    question_parent = models.ForeignKey('self', null=True, blank=True, default=None, on_delete=models.CASCADE)
-    question_statement = models.TextField()
+    statement = models.TextField()
+    learning_object = models.ForeignKey(LearningObject, null=True, blank=True)
     resolution = models.TextField(null=True, blank=True)
     level = models.CharField(max_length=1, choices = LEVEL_CHOICES, null=True, blank=True)
-    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    create_date = models.DateTimeField(auto_now_add=True)
-    credit_cost = models.PositiveIntegerField(null=False, blank=False, default=0)
+
     subjects = models.ManyToManyField(Subject, blank=True)
-    education_level = models.CharField(max_length=30, choices = EDUCATION_CHOICES, null=True, blank=True)
+    education_level = models.ForeignKey(EducationLevel, blank=True, null=True)
     year = models.PositiveIntegerField(null=True, blank=True)
     source = models.CharField(max_length=50, null=True, blank=True)
 
+    credit_cost = models.PositiveIntegerField(null=False, blank=True, default=0)
+
     tags = TaggableManager()
-    #
-    # class Meta:
-    #     ordering = ['pk']
 
     def __str__(self):
-        year_source = ''
-        if self.year != None:
-            year_source = str(self.year)
-        if self.source != None:
-            year_source = year_source + self.source
-        return u'QuestionId %d %s %s' % (self.pk, year_source, self.question_statement[:75])
+        return self.question_statement[:75]
 
 
-class Answer(models.Model):
-    question = models.ForeignKey('Question', related_name='answers', on_delete=models.CASCADE)
+class Alternative(models.Model):
+    question = models.ForeignKey(Question, related_name='alternatives', on_delete=models.CASCADE)
     answer_text = models.TextField()
     is_correct = models.BooleanField()
 
@@ -59,37 +79,37 @@ class Answer(models.Model):
         ordering = ['question', 'pk']
 
     def __str__(self):
-        return u'QuestionId: %d AnswerId: %d Correct: %s' % (
-        self.question.pk, self.pk, ('Yes' if self.is_correct else 'No'))
+        return self.answer_text[:50]
 
-
-class Question_List(models.Model):
-    question_list_header = models.CharField('Nome da Lista', max_length=200)
+class Document(models.Model):
+    name = models.CharField(max_length=200)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    questions = models.ManyToManyField(Question, through='QuestionQuestion_List', related_name='questions')
+    questions = models.ManyToManyField(Question, through='DocumentQuestion', related_name='questions')
     create_date = models.DateTimeField(auto_now_add=True)
-    secret = models.BooleanField('Lista privada')
-    cloned_from = models.ForeignKey('self', null=True, blank=True, default=None, on_delete=models.CASCADE)
-
-    class Meta:
-        ordering = ['pk']
+    secret = models.BooleanField()
 
     def __str__(self):
-        return u'ListId: %d Header: %s' % (self.pk, self.question_list_header)
+        return self.name[:50]
 
 
-class QuestionQuestion_List(models.Model):
+class DocumentQuestion(models.Model):
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    question_list = models.ForeignKey(Question_List, on_delete=models.CASCADE)
     order = models.PositiveIntegerField(null=False, blank=False)
 
     class Meta:
-        ordering = ['order']
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    credit_balance = models.PositiveIntegerField(null=False, blank=True, default=0)
-    avaiable_questions = models.ManyToManyField(Question, blank=True)
+        ordering = ['document', 'order']
 
     def __str__(self):
-        return u'Profile de %s' % (self.user.username)
+        return self.question_list_header[:50]
+
+
+class DocumentHeader(models.Model):
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    institution_name = models.CharField(max_length=200)
+    subject_name = models.CharField(max_length=50)
+    professor_name = models.CharField(max_length=200)
+    student_indicator = models.BooleanField()
+    class_indicator = models.BooleanField()
+    score_indicator = models.BooleanField()
+    date_indicator = models.BooleanField()
