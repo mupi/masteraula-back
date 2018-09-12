a480d5495686446fe7a7d348d4299ba3a59cc6e9
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-09-12 12:50:16 -0300

    Add question and user index

--- masteraula/questions/search_indexes.py
+++ masteraula/questions/search_indexes.py
@@ -10,53 +10,61 @@ from .models import Question
 import re
 import unicodedata
 
-# class QuestionIndex(indexes.SearchIndex, indexes.Indexable):
-#     text = indexes.CharField(document=True,use_template=True)
-#     author = indexes.CharField(model_attr='author')
-#     create_date = indexes.DateTimeField(model_attr='create_date')
-#     level = indexes.CharField(model_attr='level', null=True)
-#     question_statement = indexes.CharField(model_attr='question_statement')
-#     subjects = indexes.MultiValueField()
-#     tags = indexes.MultiValueField()
-#     education_level = indexes.CharField(model_attr='education_level', null=True)
-#     year = indexes.CharField(model_attr='year', null=True)
-#     source = indexes.CharField(model_attr='source', null=True)
+class QuestionIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True,use_template=True)
+    author = indexes.CharField(model_attr='author')
+    create_date = indexes.DateTimeField(model_attr='create_date')
+    
+    statement = indexes.CharField(model_attr='statement')
+    resolution = indexes.CharField(model_attr='resolution')
+    difficulty = indexes.CharField(model_attr='difficulty', null=True)
 
-#     def get_model(self):
-#         return Question
+    disciplines = indexes.MultiValueField()
+    descriptors = indexes.MultiValueField()
+    teaching_levels = indexes.MultiValueField()
+    
+    year = indexes.CharField(model_attr='year', null=True)
+    source = indexes.CharField(model_attr='source', null=True)
 
-#     def index_queryset(self, using=None):
-#         """Used when the entire index for model is updated."""
-#         return self.get_model().objects.filter(create_date__lte=timezone.now())
+    tags = indexes.MultiValueField()
 
+    def get_model(self):
+        return Question
 
-#     def prepare_question_statement(self, obj):
-#         cleaner = re.compile('<.*?>')
-#         return re.sub(cleaner, '', obj.question_statement)
+    # def index_queryset(self, using=None):
+    #     """Used when the entire index for model is updated."""
+    #     return self.get_model().objects.filter(create_date__lte=timezone.now())
 
-#     def prepare_level(self, obj):
-#         if obj.level == 'E':
-#             return 'Facil'
-#         elif obj.level == 'M':
-#             return 'Medio'
-#         elif obj.level == 'H':
-#             return 'Dificil'
-#         return ''
+    def prepare_statement(self, obj):
+        cleaner = re.compile('<.*?>')
+        return re.sub(cleaner, '', obj.statement)
 
-#     def prepare_tags(self, obj):
-#         return [tag.slug for tag in obj.tags.all()]
+    def prepare_difficulty(self, obj):
+        if obj.difficulty == 'E':
+            return 'Facil'
+        elif obj.difficulty == 'M':
+            return 'Medio'
+        elif obj.difficulty == 'H':
+            return 'Dificil'
+        return ''
 
-#     def prepare_subjects(self, obj):
-#         return [ slugify(subject.subject_name) for subject in obj.subjects.all()]
+    def prepare_tags(self, obj):
+        return [ tag.name for tag in obj.tags.all() ]
 
-#     def prepare_education_level(self, obj):
-#         return slugify(obj.education_level)
+    def prepare_tags_slugs(self, obj):
+        return [ tag.slug for tag in obj.tags.all() ]
 
-# class TagIndex(indexes.SearchIndex, indexes.Indexable):
-#     text = indexes.CharField(document=True,use_template=True)
-#     name = indexes.CharField(model_attr='name')
-#     slug = indexes.CharField(model_attr='slug')
-#     tags_auto = indexes.EdgeNgramField(model_attr='slug')
+    def prepare_disciplines(selfy, obj):
+        return [ discipline.name for discipline in obj.disciplines.all() ]
 
-#     def get_model(self):
-#         return Tag
+    def prepare_teaching_levels(self, obj):
+        return [ teaching_level.name for teaching_level in obj.teaching_levels.all() ]
+
+class TagIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True,use_template=True)
+    name = indexes.CharField(model_attr='name')
+    slug = indexes.CharField(model_attr='slug')
+    tags_auto = indexes.EdgeNgramField(model_attr='slug')
+
+    def get_model(self):
+        return Tag
