95356c36a360b8732be7e31392fd2ab5d9bcd39b
GlauciaSchnoeller <glauciaschnoellers@gmail.com>
2018-09-20 11:39:45 -0300

    Add include router

--- masteraula/users/urls.py
+++ masteraula/users/urls.py
@@ -4,15 +4,11 @@ from __future__ import absolute_import, unicode_literals
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
-
 from . import views
 
 router = DefaultRouter()
 router.register(r'cities', views.CityViewSet, base_name='cities-list')
 router.register(r'states', views.StateViewSet, base_name='states-list')
-
-
-
 
 urlpatterns = [
     url(r'^', include(router.urls)),
@@ -20,6 +16,5 @@ urlpatterns = [
         regex=r'^resend_confirmation_email$',
         view=views.UserConfirmationEmailView.as_view(),
         name='list'
-    ),
-    
+    ),  
 ]
