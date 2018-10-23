31161f3d3fe39a4186f1a869dc75fcf800811120
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-10-23 15:19:36 -0300

    Add ID field and [IMAGEM] placeholder at img tags

--- masteraula/questions/management/commands/export_questions.py
+++ masteraula/questions/management/commands/export_questions.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
     help = ("Output the specified model as CSV: ./manage.py export_questions > questions.csv")
     args = '[Question]'
 
-    fields = ['Autor','Origem','Ano','Grau de dificuldade','Disciplinas','Nível de ensino','Enunciado','Resolução','Alternativa Correta','Alternativa A','Alternativa B','Alternativa C','Alternativa D','Alternativa E']
+    fields = ['ID', 'Autor','Origem','Ano','Grau de dificuldade','Disciplinas','Nível de ensino','Enunciado','Resolução','Alternativa Correta','Alternativa A','Alternativa B','Alternativa C','Alternativa D','Alternativa E']
     writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL)
     writer.writerow(fields)
     
@@ -33,7 +33,7 @@ class Command(BaseCommand):
             answer = Alternative.objects.filter(question=q)
             disciplines = Discipline.objects.filter(question=q)
             teaching_levels = TeachingLevel.objects.filter(question=q)
-            statement = remove_tags(q.statement)
+            statement = remove_tags(rename_image_tags(q.statement))
             
             if q.difficulty == 'M':
                 difficulty = "Médio"
@@ -52,18 +52,20 @@ class Command(BaseCommand):
 
             for a in answer:      
                 if a.is_correct:
-                    resp = remove_tags(a.text)
+                    resp = remove_tags(rename_image_tags(a.text))
 
-            fields = [q.author, q.source, q.year, difficulty, disc, teaching_level, statement, q.resolution, resp]
+            fields = [q.id, q.author, q.source, q.year, difficulty, disc, teaching_level, statement, q.resolution, resp]
 
             for a in answer:
-                alternative = remove_tags(a.text)
+                alternative = remove_tags(rename_image_tags(a.text))
                 fields += [alternative]
                 
             writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL)
             writer.writerow(fields)
 
-    
+def rename_image_tags(text):
+    TAG_RE = re.compile("<img(.)*?src=\"(.*?)\"(.)*?>")
+    return TAG_RE.sub('[IMAGEM]', text)
 
 def remove_tags(text):
     TAG_RE = re.compile(r'<[^>]+>')
