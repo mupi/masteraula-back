43735f1a842d24c2c98b4407d017df513f03932b
GlauciaSchnoeller <glauciaschnoellers@gmail.com>
2018-10-23 11:49:51 -0300

    Script to export questions and fix some questions

---
+++ masteraula/questions/management/commands/export_questions.py
@@ -0,0 +1,66 @@
+from django.core.management.base import BaseCommand, CommandError
+from masteraula.questions.models import Question, Alternative, TeachingLevel, Discipline
+from django.apps import apps
+import csv
+import sys
+import re
+
+
+"""
+ Prints CSV of fields of a model.
+"""
+
+class Command(BaseCommand):
+    help = ("Output the specified model as CSV: ./manage.py export_questions > questions.csv")
+    args = '[Question]'
+
+    fields = ['Autor','Origem','Ano','Grau de dificuldade','Disciplinas','Nível de ensino','Enunciado','Resolução','Alternativa Correta','Alternativa A','Alternativa B','Alternativa C','Alternativa D','Alternativa E']
+    writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL)
+    writer.writerow(fields)
+    
+    def handle(self, *app_labels, **options):
+        
+        question = Question.objects.all()
+       
+        for q in question:
+            
+            answer = Alternative.objects.filter(question=q)
+            disciplines = Discipline.objects.filter(question=q)
+            teaching_levels = TeachingLevel.objects.filter(question=q)
+            statement = remove_tags(q.statement)
+            
+            if q.difficulty == 'M':
+                difficulty = "Médio"
+            
+            if q.difficulty == 'E':
+               difficulty = "Fácil"
+            
+            if q.difficulty == 'H':
+               difficulty = "Difícil"
+                        
+            for d in disciplines:
+                disc = d.name      
+                
+            for t in teaching_levels:
+                teaching_level = t.name
+
+            for a in answer:      
+                if a.is_correct:
+                    resp = remove_tags(a.text)
+
+            fields = [q.author, q.source, q.year, difficulty, disc, teaching_level, statement, q.resolution, resp]
+
+            for a in answer:
+                alternative = remove_tags(a.text)
+                fields += [alternative]
+                
+            writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL)
+            writer.writerow(fields)
+
+    
+
+def remove_tags(text):
+    TAG_RE = re.compile(r'<[^>]+>')
+    return TAG_RE.sub('', text)
+               
+        
