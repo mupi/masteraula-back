bd465f4c3032a0ca834d8908296ef88734368026
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-10-25 15:51:08 -0300

    Upload images to custom folder as the name of the folder_name

--- masteraula/questions/models.py
+++ masteraula/questions/models.py
@@ -32,16 +32,18 @@ class TeachingLevel(models.Model):
         return self.name
 
 class LearningObject(models.Model):
+    def get_upload_file_name(learning_object,filename):
+        folder_name = learning_object.folder_name if learning_object.folder_name else 'default'
+        return u'masteraula/%s/%s' % (folder_name, filename)
+
     # name = models.CharField(max_length=100, null=False, blank=False)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to=get_upload_file_name)
+    folder_name = models.CharField(max_length=100, null=True, blank=True)
     text = models.TextField(null=True, blank=True)
 
     tags = TaggableManager(blank=True)
-
-    # def __str__(self):
-    #     return self.name
 
 
 class Descriptor(models.Model):
