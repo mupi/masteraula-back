72489b980de85659daf326e8efd37f528a01da47
GlauciaSchnoeller <glauciaschnoellers@gmail.com>
2019-03-13 15:29:14 -0300

    Inserted new field in user and search of the django admin

--- masteraula/questions/admin.py
+++ masteraula/questions/admin.py
@@ -134,7 +134,7 @@ class HeaderModelAdmin(admin.ModelAdmin):
 class SearchModelAdmin(ExportMixin, admin.ModelAdmin):
     resource_class = SearchResource
     raw_id_fields = ('user',)
-    list_display = ('id', 'user', 'term', 'date_search')
+    list_display = ('id', 'user_id', 'user', 'term', 'date_search')
     search_fields = ['id', 'user__name', 'term', 'date_search']
     list_per_page = 100
 
