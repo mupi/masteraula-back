7b4eda88f6204444ae17df361b32e30857f5046d
GlauciaSchnoeller <glauciaschnoellers@gmail.com>
2019-03-19 13:43:42 -0300

    Create new function to export number of documents of the users

--- masteraula/reports/urls.py
+++ masteraula/reports/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.ReportsView.as_view(), name='reports_home'),
     url(r'^uncategorized_questions/$', views.UncategorizedTagsView.as_view(), name='uncategorized_questions'),
+    url(r'^number_documents/$', views.NumberDocumentsView.as_view(), name='number_documents'),
 ]
--- masteraula/reports/views.py
+++ masteraula/reports/views.py
@@ -4,10 +4,12 @@ from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 from django.views.decorators.http import require_http_methods
 
-from masteraula.questions.models import Question, Discipline
+from masteraula.questions.models import Question, Discipline, Document, User
+
 from django.views import View
 
 class ReportsView(LoginRequiredMixin, View):
@@ -52,3 +54,46 @@ class UncategorizedTagsView(LoginRequiredMixin, View):
         )
         response['Content-Disposition'] = 'attachment; filename="relatorio.csv"'
         return response
+
+class NumberDocumentsView(LoginRequiredMixin, View):
+    login_url = '/admin/login/'
+    template_name = 'reports/number_documents.html'
+    
+
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            return redirect('/admin/login/?next=%s' % request.path)
+
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):      
+        if not request.user.is_superuser:
+            return redirect('/admin/login/?next=%s' % request.path)
+        
+        data = ''            
+        data = 'Usuário,' + 'Provas Ativas, '+ 'Provas Criadas,' + '\n'  
+            
+        if request.POST['id_users'] != '':
+            users = request.POST['id_users']
+
+        else:
+            user_id = User.objects.all()
+            users = [i.id for i in user_id]  
+        
+        if type(users) is not list:
+            for user in users.split(","):
+                documents = Document.objects.filter(owner_id = user).count()
+                documents_active = Document.objects.filter(owner_id = user, disabled = False).count()
+                data = data + str(user) + ',' + str(documents_active) + ',' + str(documents) + '\n'    
+        
+        else:
+            for user in users:
+                documents = Document.objects.filter(owner_id = user).count()
+                documents_active = Document.objects.filter(owner_id = user, disabled = False).count()
+                data = data + str(user) + ',' + str(documents_active) + ',' + str(documents) + '\n'
+    
+        response = HttpResponse(data)
+
+        response['Content-Disposition'] = 'attachment; filename="relatorio_provas.csv"'
+        return response
+
