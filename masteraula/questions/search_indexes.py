c29f41073e400d88dff1d90a54c923bd9a8b44e7
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-05-07 12:27:59 -0300

    Prepare new search indexes

--- masteraula/questions/search_indexes.py
+++ masteraula/questions/search_indexes.py
@@ -12,41 +12,29 @@ import re
 import unicodedata
 
 class QuestionIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True,use_template=True)
-    author = indexes.CharField(model_attr='author')
-    #create_date = indexes.DateTimeField(model_attr='create_date')
-    
-    statement = indexes.CharField(model_attr='statement')
-    learning_objects = indexes.MultiValueField(indexed=True)
-    resolution = indexes.CharField(model_attr='resolution')
-    difficulty = indexes.CharField(model_attr='difficulty', null=True)
+    text = indexes.CharField(document=True,use_template=True, boost=0.1)
 
-    disciplines = indexes.MultiValueField(indexed=True)
-    descriptors = indexes.MultiValueField(indexed=True)
-    teaching_levels = indexes.MultiValueField(indexed=True)
-    topics = indexes.MultiValueField(indexed=True)
-    
-    year = indexes.CharField(model_attr='year', null=True)
-    source = indexes.CharField(model_attr='source', null=True)
+    topics = indexes.CharField(boost=100)
+    tags = indexes.CharField(boost=50)
+    learning_objects_tags = indexes.CharField(boost=30)
 
-    tags = indexes.MultiValueField(indexed=True)
+    disciplines = indexes.MultiValueField()
+    teaching_levels = indexes.MultiValueField()
+    year = indexes.CharField(model_attr='year')
+    source = indexes.CharField(model_attr='source')
+    difficulty = indexes.CharField()
 
     def get_model(self):
         return Question
+    
+    def prepare_topics(self, obj):
+        return ' '.join([ topic.name for topic in obj.topics.only('name') ])
 
-    # def index_queryset(self, using=None):
-    #     """Used when the entire index for model is updated."""
-    #     return self.get_model().objects.filter(create_date__lte=timezone.now())
+    def prepare_tags(self, obj):
+        return ' '.join([ tag.name for tag in obj.tags.only('name') ])
 
-    def prepare_statement(self, obj):
-        cleaner = re.compile('<.*?>')
-        return re.sub(cleaner, '', obj.statement)
-
-    def prepare_learning_objects(self, obj):
-        cleaner = re.compile('<.*?>')
-        for learning_objects in obj.learning_objects.all():
-            if learning_objects.text is not None:
-                return [re.sub(cleaner, '', learning_objects.text)]      
+    def prepare_learning_objects_tags(self, obj):
+        return ' '.join([ tag.name for lo in obj.learning_objects.all() for tag in lo.tags.only('name') ])
 
     def prepare_difficulty(self, obj):
         if obj.difficulty == 'E':
@@ -57,23 +45,8 @@ class QuestionIndex(indexes.SearchIndex, indexes.Indexable):
             return 'Dificil'
         return ''
 
-    def prepare_tags(self, obj):
-        return [ tag.name for tag in obj.tags.all() ]
-
-    def prepare_tags_slugs(self, obj):
-        return [ tag.slug for tag in obj.tags.all() ]
-
     def prepare_disciplines(self, obj):
-        return [ discipline.pk for discipline in obj.disciplines.all() ]
+        return [ discipline.pk for discipline in obj.disciplines.only('pk') ]
 
     def prepare_teaching_levels(self, obj):
-        return [ teaching_level.pk for teaching_level in obj.teaching_levels.all() ]
-
-class TagIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True,use_template=True)
-    name = indexes.CharField(model_attr='name')
-    slug = indexes.CharField(model_attr='slug')
-    tags_auto = indexes.EdgeNgramField(model_attr='slug')
-
-    def get_model(self):
-        return Tag
+        return [ teaching_level.pk for teaching_level in obj.teaching_levels.only('pk') ]
--- masteraula/questions/templatetags/search_helpers.py
+++ masteraula/questions/templatetags/search_helpers.py
@@ -1,9 +1,17 @@
 from django import template
 import unicodedata
+import re
 
 def stripaccents(value):
     if value is not None:
         return ''.join((c for c in unicodedata.normalize('NFD', value) if unicodedata.category(c) != 'Mn'))
 
+def prepare_document(value):
+    value = re.sub('<.*?>', '', value.lower())
+    value = ' '.join(list(set(re.findall('\w\w\w+', value))))
+    return ''.join((c for c in unicodedata.normalize('NFD', value) if unicodedata.category(c) != 'Mn'))
+
+
 register = template.Library()
 register.filter("stripaccents", stripaccents)
+register.filter("prepare_document", prepare_document)
