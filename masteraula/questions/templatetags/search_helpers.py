9aa6199166a8e9026f862a40d1ec0fe0a3d03177
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-05-08 12:57:40 -0300

    Strip accents and convert to lower

--- masteraula/questions/templatetags/search_helpers.py
+++ masteraula/questions/templatetags/search_helpers.py
@@ -1,16 +1,16 @@
 from django import template
+from haystack.inputs import Clean
 import unicodedata
 import re
 
 def stripaccents(value):
     if value is not None:
-        return ''.join((c for c in unicodedata.normalize('NFD', value) if unicodedata.category(c) != 'Mn'))
+        return ''.join((c for c in unicodedata.normalize('NFD', value.lower()) if unicodedata.category(c) != 'Mn'))
 
 def prepare_document(value):
     value = re.sub('<.*?>', '', value.lower())
     value = ' '.join(list(set(re.findall('\w\w\w+', value))))
     return ''.join((c for c in unicodedata.normalize('NFD', value) if unicodedata.category(c) != 'Mn'))
-
 
 register = template.Library()
 register.filter("stripaccents", stripaccents)
