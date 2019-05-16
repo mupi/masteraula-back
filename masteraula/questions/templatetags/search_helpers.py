4ac3fe98bf59079182e4e5b29adc081452418668
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-05-16 15:47:34 -0300

    Check for values at template tags

--- masteraula/questions/templatetags/search_helpers.py
+++ masteraula/questions/templatetags/search_helpers.py
@@ -6,11 +6,14 @@ import re
 def stripaccents(value):
     if value is not None:
         return ''.join((c for c in unicodedata.normalize('NFD', value.lower()) if unicodedata.category(c) != 'Mn'))
+    return ''
 
 def prepare_document(value):
-    value = re.sub('<.*?>', '', value.lower())
-    value = ' '.join(list(set(re.findall('\w\w\w+', value))))
-    return ''.join((c for c in unicodedata.normalize('NFD', value) if unicodedata.category(c) != 'Mn'))
+    if value is not None:
+        value = re.sub('<.*?>', '', value.lower())
+        value = ' '.join(list(set(re.findall('\w\w\w+', value))))
+        return ''.join((c for c in unicodedata.normalize('NFD', value) if unicodedata.category(c) != 'Mn'))
+    return ''
 
 register = template.Library()
 register.filter("stripaccents", stripaccents)
