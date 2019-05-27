9b885caeb4edf866607a38ce08e9e8a155c276ca
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-05-27 09:33:14 -0300

    Add check duplicates command

--- masteraula/questions/management/commands/check_duplicates.py
+++ masteraula/questions/management/commands/check_duplicates.py
@@ -13,6 +13,56 @@ from threading import Thread
 import csv
 import os
 
+def check_duplicates(json_questions, discipline):
+    duplicates = []
+
+    json_questions_stm = []
+
+    for question in json_questions:
+        statement = '{} {}'.format(' '.join(question['statement_p1']), ' '.join(question['statement_p2']))
+        json_questions_stm.append((only_key_words(statement),
+                                    question['id_enem'],
+                                    statement))
+    print('prepared all new questions')
+    
+    all_questions = []
+    all_questions_dict = {}
+    for question in Question.objects.filter(disciplines__name__in=[discipline]):
+        all_questions.append(only_key_words(question.statement))
+        all_questions_dict[all_questions[-1]] = question.pk
+    print('prepared all questions')
+
+    res = []
+    for question_stm in json_questions_stm:
+        res.append((len(question_stm[0]), question_stm[0], question_stm[1], question_stm[2]))
+
+    res.sort()
+    scores = []
+    for i, question_stm in enumerate(res):
+        print('Questao {}'.format(i + 1))
+        if question_stm[0]:
+            choices = process.extractOne(question_stm[1], all_questions, scorer=fuzz.token_sort_ratio)
+            scores.append([choices[1], all_questions_dict[choices[0]], question_stm[3], question_stm[2]])
+        else:
+            scores.append([-1, '', question_stm[3], question_stm[2]])
+
+    scores.sort()
+    
+    for i, score in enumerate(scores):
+        if score[0] < 0:
+            print('\nQuestao {}'.format(i + 1))
+            print('questao muito simples')
+            print('questao comparada: {} {}'.format(score[3], score[2]))
+            duplicates.append(score[3])
+        elif score[0] >= 65:
+            print('\nQuestao {}'.format(i + 1))
+            print('score: {}'.format(score[0]))
+            print('questao banco:{} {}'.format(score[1], Question.objects.get(id=score[1]).statement))
+            print('questao comparada: {} {}'.format(score[3], score[2]))
+            duplicates.append(score[3])
+
+    return duplicates
+
 class Command(BaseCommand):
     help = 'Check if there is a duplicate question from the file'
 
@@ -27,35 +77,9 @@ class Command(BaseCommand):
         with open(filename, 'r') as f:
             json_questions = json.loads(f.read().replace(' ', ''))
 
-        json_questions_stm = []
-        for question in json_questions:
-            statement = '{} {}'.format(question['statement_p1'], question['statement_p2'])
-            json_questions_stm.append((only_key_words(statement),
-                                     question['id_enem'],
-                                     statement))
-        print('prepared all new questions')
-        
-        all_questions = []
-        all_questions_dict = {}
-        for question in Question.objects.filter(disciplines__name__in=[DISCIPLINE]):
-            all_questions.append(only_key_words(question.statement))
-            all_questions_dict[all_questions[-1]] = question.pk
-        print('prepared all questions')
+        duplicates = check_duplicates(json_questions, DISCIPLINE)
 
-        res = []
-        for question_stm in json_questions_stm:
-            res.append((len(question_stm[0]), question_stm[0], question_stm[1], question_stm[2]))
+        with open('duplicates.json', 'w') as f:
+            f.write(json.dumps(duplicates))
 
-        res.sort()
-        scores = []
-        for i, question_stm in enumerate(res):
-            print('Questao {}'.format(i + 1))
-            if question_stm[0]:
-                choices = process.extractOne(question_stm[1], all_questions, scorer=fuzz.token_sort_ratio)
-                choosen = all_questions_dict[choices[0]], choices[1]
-                scores.append([i, choosen, question_stm[3]])
-            else:
-                scores.append([i, '', question_stm[3]])
-
-        
-        print(scores)
+        print('Ok')
