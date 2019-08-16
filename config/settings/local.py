30fa875c664f0139937579e9d97cd16cdd4ae065
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-08-16 12:34:03 -0300

    Add silk to project

--- config/settings/local.py
+++ config/settings/local.py
@@ -57,8 +57,11 @@ CACHES = {
 
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
-MIDDLEWARE += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-INSTALLED_APPS += ('debug_toolbar', )
+MIDDLEWARE += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'silk.middleware.SilkyMiddleware',
+)
+INSTALLED_APPS += ('debug_toolbar', 'silk')
 
 INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', ]
 # tricks to have debug toolbar when developing with docker
