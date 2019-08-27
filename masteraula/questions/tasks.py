2a9dd7077d08ede03c64edab55a731eda031b027
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-08-27 12:51:44 -0300

    Add retry option when failling updating index

--- masteraula/questions/tasks.py
+++ masteraula/questions/tasks.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, unicode_literals
+from whoosh.index import LockError
 from celery import shared_task
 
 from .search_indexes import QuestionIndex, LearningObjectIndex
@@ -11,25 +12,28 @@ def update_or_remove_question(question):
     else:
         QuestionIndex().update_object(instance=question)
 
-@shared_task(name="update_question_index")
+@shared_task(name="update_question_index", autoretry_for=(LockError,),
+    retry_kwargs={'max_retries': 8}, retry_backoff=True)
 def update_question_index(question):
     try:
         question = Question.objects.get_questions_update_index().get(id=question)
-        update_or_remove_question(question)
     except:
-        pass
+        return
+    update_or_remove_question(question)
     
-@shared_task(name="update_learning_object_index")
+@shared_task(name="update_learning_object_index", autoretry_for=(LockError,),
+    retry_kwargs={'max_retries': 8}, retry_backoff=True)
 def update_learning_object_index(learningObject):
     try:
         lo = LearningObject.objects.get_objects_update_index().get(id=learningObject)
-        LearningObjectIndex().update_object(instance=lo)
-        for question in lo.questions.all():
-            update_or_remove_question(question)
     except:
-        pass
+        return
+    LearningObjectIndex().update_object(instance=lo)
+    for question in lo.questions.all():
+        update_or_remove_question(question)
 
-@shared_task(name="update_questions_topic")
+@shared_task(name="update_questions_topic", autoretry_for=(LockError,),
+    retry_kwargs={'max_retries': 8}, retry_backoff=True)
 def update_questions_topic(topic):
     questions = Question.objects.get_questions_update_index().filter(topics__id=topic)
     for question in questions:
