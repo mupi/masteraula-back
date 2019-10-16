2c936bcced09ab7592d6cdcdb9066a51d8e6a61d
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-10-16 16:54:54 -0300

    Add donwload permission, retrict 3 dowloads, clear old code

--- masteraula/questions/permissions.py
+++ masteraula/questions/permissions.py
@@ -1,4 +1,10 @@
 from rest_framework import permissions
+from django.db.models import Q
+
+from masteraula.users.models import Subscription
+from masteraula.questions.models import DocumentDownload
+
+import datetime
 
 class QuestionPermission(permissions.BasePermission):
     """Só poderá editar a questão se o usuário for autenticado"""
@@ -26,7 +32,7 @@ class DocumentsPermission(permissions.BasePermission):
     - Qualquer usuário logado pode criar um documento;
     - Só podem editar um documento o autor desse documento e o super_usuario."""
 
-    def has_permission(self, request, view, obj=None):
+    def has_permission(self, request, view):
         if request.user.is_authenticated:
             return True
 
@@ -42,7 +48,7 @@ class HeaderPermission(permissions.BasePermission):
     - Qualquer usuário logado pode criar um cabeçalho;
     - Só pode editar e visualizar um cabeçalho o autor e o super_usuario."""
 
-    def has_permission(self, request, view, obj=None):
+    def has_permission(self, request, view):
         if request.user.is_authenticated:
             return True
        
@@ -51,78 +57,18 @@ class HeaderPermission(permissions.BasePermission):
            return True
         return obj.owner == request.user
 
-"""from rest_framework import permissions
+class DocumentDownloadPermission(permissions.BasePermission):
+    """3 download por usuário se não for premium"""
 
-class Question_ListPermission(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        # Qualquer usuario pode fazer a listagem dos dados
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Apenas super-usuarios e usuarios logados podem criar listas
-        if request.user.is_superuser:
-            return True
-        return request.user.is_authenticated()
-
-    def has_object_permission(self, request, view, obj):
-        # Apenas super-usuarios e donos da listas podem editar
-        if request.user.is_superuser:
-            return True
-        if obj.owner == request.user:
-            return True
-        # Se a lista for privada, negar acesso a outras pessoas
-        if obj.secret:
-            return False
-        # Habilita somente métodos seguros
-        return request.method in permissions.SAFE_METHODS
-
-    
-class QuestionPermission(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        # Qualquer usuario pode fazer a listagem dos dados
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Apenas super-usuarios e usuarios logados podem criar questoes
-        if request.user.is_superuser:
-            return True
-        return request.user.is_authenticated()
-
-    def has_object_permission(self, request, view, obj):
-        if view.action == 'generate_list' and obj.owner != request.user:
+    def has_permission(self, request, view, obj=None):
+        if not request.user.is_authenticated:
             return False
 
-        # Qualquer usuario pode ver as questoes
-        if request.method in permissions.SAFE_METHODS:
-            return True
+        now = datetime.datetime.now()
+        premium = Subscription.objects.filter(Q(user=request.user) & Q(expiration_date__gt=now)).count()
 
-        # Somente super_usuarios e os respectivos autores das questoes podem editar
-        if request.user.is_superuser:
+        if premium:
             return True
-        return obj.author == request.user
-
-class UserPermission(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        # Qualquer usuario pode fazer a listagem dos dados
-        if request.method in permissions.SAFE_METHODS:
+        if DocumentDownload.objects.filter(Q(user=request.user) & Q(download_date__month=now.month)).count() < 3:
             return True
-
-        # super-usuarios tem privilegio total
-        if request.user.is_superuser:
-            return True
-        # Somente pode criar novos usuarios pessoas nao logadas
-        if view.action == 'create':
-            return not request.user.is_authenticated()
-        # garante acesso aos demais metodos nao SAFE
-        return request.user.is_authenticated()
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        if request.user.is_superuser:
-            return True
-        return obj == request.user"""
+        return False
