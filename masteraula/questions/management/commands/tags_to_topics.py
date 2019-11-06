9028d0a02cbff6c19cdbec13ced1e787b948dbeb
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-11-06 14:37:38 -0300

    finished tag to topics script

--- masteraula/questions/management/commands/tags_to_topics.py
+++ masteraula/questions/management/commands/tags_to_topics.py
@@ -1,5 +1,6 @@
 import difflib
 import unidecode
+import csv
 
 from fuzzywuzzy import process, fuzz
 from taggit.models import Tag, TaggedItem
@@ -45,7 +46,7 @@ def prepare_tagged_questions(questions):
 
     return tagged_questions
 
-def tag_to_topics(discipline, choices, name_topics):
+def tags_to_topics(discipline, choices, name_topics):
     with open('changed_topics_{}.csv'.format(discipline.name), 'a') as matches:
         questions = Question.objects.filter(Q(tags__isnull=False)&Q(disciplines__in=[discipline])&Q(disabled=False)).distinct().prefetch_related('tags', 'topics').order_by('id')
 
@@ -71,14 +72,70 @@ def tag_to_topics(discipline, choices, name_topics):
                 TaggedItem.objects.filter(tag=tag, object_id__in=tagged_questions[t], content_type=ct).delete()
                 matches.write('{},"{}",{}\n'.format(t, res[0], ' '.join(str(a) for a in tagged_questions[t])))
 
+def tags_to_topics_filename(reader, discipline):
+    with open('changed_topics_{}.csv'.format(discipline.name), 'a') as matches:
+        questions = Question.objects.filter(Q(tags__isnull=False)&Q(disciplines__in=[discipline])&Q(disabled=False)).distinct().prefetch_related('tags', 'topics').order_by('id')
+
+        _, name_topics = prepare_choices_and_topics()
+        id_questions = prepare_id_questions(questions)
+        tagged_questions = prepare_tagged_questions(questions)
+
+        for row in reader:
+            if (row[0].strip() == ''):
+                continue
+            t = row[0].strip()
+            res = eval(row[1])[0]
+            print('{} -> {}'.format(t, res[0]))
+
+            try:
+                tag = Tag.objects.get(name=t)
+                topics = name_topics[res[0]]
+            except:
+                print('Tag {} or topic {} does not exist'.format(t, res[0]))
+                continue
+
+            if t not in tagged_questions:
+                print('Tag {} does not contain any question'.format(t))
+                continue
+
+            for question_id in tagged_questions[t]:
+                question = id_questions[question_id]
+
+                for topic in topics:
+                    question.topics.add(topic)
+                
+                print(question.pk)
+            
+            TaggedItem.objects.filter(tag=tag, object_id__in=tagged_questions[t], content_type=ct).delete()
+            matches.write('{},"{}",{}\n'.format(t, res[0], ' '.join(str(a) for a in tagged_questions[t])))
+
 class Command(BaseCommand):
     help = 'Tranform tag into topic, if scrores 100, o filename and discipline passed'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--filename', type=str, default='')
+        parser.add_argument('--discipline', type=str, default='')
+
     def handle(self, *args, **options):
+        if options['filename'] and options['discipline']:
+            try:
+                discipline = Discipline.objects.get(name=options['discipline'])
+            except:
+                print('Discipline {} does not exist'.format(options['discipline']))
+                exit(1)
+
+            with open(options['filename'], 'r') as csvfile:
+                reader = csv.reader(csvfile, delimiter=',')
+                tags_to_topics_filename(reader, discipline)
+            return
+
+        if options['filename'] or options['discipline']:
+            print('Should pass filename with discipline')
+            exit(1)
         
         choices, name_topics = prepare_choices_and_topics()
         for discipline in Discipline.objects.all():
             print(discipline.name)
-            tag_to_topics(discipline, choices, name_topics)
+            tags_to_topics(discipline, choices, name_topics)
 
         print('Ok')
