b163bba961eb73a4782851fd0712812ef2cfcdbc
Glaucia S. Santos <glaucia.schnoeller@outlook.com>
2020-07-24 09:48:49 -0300

    Changes in similarity of questions and activities

--- masteraula/questions/similarity.py
+++ masteraula/questions/similarity.py
@@ -1,5 +1,7 @@
 import warnings
 
+from collections import OrderedDict
+
 from taggit.models import Tag
 from .models import Question, Activity
 from django.conf import settings
@@ -9,13 +11,16 @@ class RelatedQuestions():
 
     def similar_questions(self, question, activity=False):
         list_question = []
-        questions = Question.objects.filter(topics__in=question.get_all_topics()).order_by('?')
-        disciplines = Question.objects.filter(disciplines__in=question.disciplines.all()).exclude(id__in=questions).order_by('?')
+        questions = Question.objects.filter(topics__in=question.get_all_topics(), disabled=False).order_by('?')
+        disciplines = Question.objects.filter(disciplines__in=question.disciplines.all(), disabled=False).exclude(id__in=questions).order_by('?')
 
         if activity == False:
             questions = questions.exclude(id=question.id)
             disciplines = disciplines.exclude(id=question.id)
 
+        questions = list(OrderedDict.fromkeys(questions.values_list('id', flat=True))) 
+        disciplines = list(OrderedDict.fromkeys(disciplines.values_list('id', flat=True))) 
+     
         if not questions:
             questions = disciplines[:8]
 
@@ -28,14 +33,17 @@ class RelatedQuestions():
                 questions += disciplines[:8]
 
         # Get activities similar
-        activities = Activity.objects.filter(topics__in=question.get_all_topics()).order_by('?')
-        disciplines_act = Activity.objects.filter(disciplines__in=question.disciplines.all()).exclude(id__in=activities).order_by('?')
+        activities = Activity.objects.filter(topics__in=question.get_all_topics(), disabled=False).order_by('?')
+        disciplines_act = Activity.objects.filter(disciplines__in=question.disciplines.all(), disabled=False).exclude(id__in=activities).order_by('?')
         list_activity = []
 
         if activity == True:
             activities = activities.exclude(id=question.id)
             disciplines_act = disciplines_act.exclude(id=question.id)
         
+        activities = list(OrderedDict.fromkeys(activities.values_list('id', flat=True))) 
+        disciplines_act = list(OrderedDict.fromkeys(disciplines_act.values_list('id', flat=True))) 
+
         if not activities:
             activities = disciplines_act[:8]
 
@@ -48,7 +56,7 @@ class RelatedQuestions():
                 activities += disciplines_act[:8]
 
         for item in activities[:4]:
-            list_activity.append(item.id)
+            list_activity.append(item)
 
         if len(activities) < 4:
             questions = questions[:(8 - len(activities))]
@@ -56,6 +64,6 @@ class RelatedQuestions():
             questions = questions[:4]
 
         for item in questions:
-            list_question.append(item.id)
+            list_question.append(item)
 
         return list_question, list_activity
